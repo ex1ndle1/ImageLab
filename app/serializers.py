a6e280c14f9f1106012e9d7943fb6389b7ab07b3
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-01-11 20:53:53 +0500

    .

--- app/serializers.py
+++ app/serializers.py
@@ -10,6 +10,21 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class UsersSerializer(serializers.ModelSerializer):
     avatar = ImageSerializer(read_only=True)
+    age_status = serializers.SerializerMethodField()
+    password_quality = serializers.SerializerMethodField()
     class Meta :
         model = models.User
-        fields = ('id', 'username', 'age', 'avatar')
+        fields = ('id', 'username', 'age', 'avatar', 'age_status', 'password_quality')
+
+
+    def get_age_status(self, obj):
+
+        if obj.age < 18:
+             return 'User not adult yet'
+        return 'User is adult'
+
+    def get_password_quality(self, obj):
+        if len(obj.password) < 8:
+            return 'Weak Password'
+        print(obj.password)
+        return 'Strong password'
